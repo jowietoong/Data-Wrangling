bbb04c0743ec7c01100d534859de357a4f32a3c7
jowietoong <57376455+jowietoong@users.noreply.github.com>
2019-11-30 11:29:55 +0800

    Add files via upload

---
+++ 9.2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {'Box': ['Box1', 'Box1', 'Box1', 'Box2', 'Box2', 'Box2'], 'Dimension':
+    ['Length', 'Width', 'Height', 'Length', 'Width', 'Height'], 'Value': [6,4,2,5,3,4]}
+box = pd.DataFrame (data, columns = ['Box', 'Dimension', 'Value'])
+tidy = box.pivot_table(index = 'Box', columns = 'Dimension', values = 'Value')
+V = [48,60]
+tidy['Volume'] = V
+    
